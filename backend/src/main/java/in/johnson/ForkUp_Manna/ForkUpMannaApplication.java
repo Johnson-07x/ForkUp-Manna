ff2c3fdc9f37acc5b178fe1a4aea95dcda24ae05
Johnson A J <johnson.07x@gmail.com>
2026-05-01 12:56:57 +0530

    chore: setup backend and frontend dirs

---
+++ backend/src/main/java/in/johnson/ForkUp_Manna/ForkUpMannaApplication.java
@@ -0,0 +1,13 @@
+package in.johnson.ForkUp_Manna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForkUpMannaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForkUpMannaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/in/johnson/ForkUp_Manna/ForkUpMannaApplicationTests.java
@@ -0,0 +1,13 @@
+package in.johnson.ForkUp_Manna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForkUpMannaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
